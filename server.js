7b0bcb6e138e3c635a25de0242e645bdcb1a39fe
Jana Scheuble <janascheuble@Janas-MacBook-Air.local>
2020-04-16 12:17:34 -0700

    initial commit, file setup

---
+++ server.js
@@ -0,0 +1,12 @@
+const express = require("express");
+const helmet = require("helmet");
+
+const server = express();
+
+server.use(express.json());
+
+server.get("/", (req, res) => {
+  res.status(200).json({ message: "api running" });
+});
+
+module.exports = server;
